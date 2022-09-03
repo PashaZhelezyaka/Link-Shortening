2b2d70a46a257e8315cbe7cd33fb29db6b6b067b
PashaZhelezyaka <pilipenkopv1991@gmail.com>
2022-09-03 23:21:48 +0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login-page/login.component';
+import { RegisterComponent } from './register-page/register.component';
+import { HomeComponent } from "./main-page/home.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
