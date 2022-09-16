e0813d874a2c14acb7e0dc5dd8e8a804ac460a24
PashaZhelezyaka <pilipenkopv1991@gmail.com>
2022-09-17 01:02:44 +0300

    added guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from "./login-page/login.component";
 import { RegisterComponent } from "./register-page/register.component";
 import { MainComponent } from "./main-page/main.component";
 import { NotFoundPageComponent } from "./not-found-page/not-found-page.component";
+import { AuthGuardGuard } from "./shared/auth-guard.guard";
 
 const routes: Routes = [
   { path:'login', component: LoginComponent },
   { path:'register', component: RegisterComponent },
-  { path:'main', component: MainComponent },
+  { path:'main', component: MainComponent, canActivate: [AuthGuardGuard] },
   { path:'', redirectTo: 'login', pathMatch: 'full' },
   { path:'**', component: NotFoundPageComponent }
     ]
--- src/app/login-page/login.component.ts
+++ src/app/login-page/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   submitted: boolean = false;
 
 
-  constructor(public auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
